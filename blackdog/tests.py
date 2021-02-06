b7c961d0be94377d2627683882fc40e01f420142
UncleGoogle <mieszkoziemowit@gmail.com>
2021-02-06 22:35:25 +0100

    add dummy test&view for index

--- blackdog/tests.py
+++ blackdog/tests.py
@@ -2,7 +2,10 @@ from django.urls import reverse
 
 
 def test_root_view(client):
-    url = reverse('/')
+    url = reverse('index')
     response = client.get(url)
     assert response.status_code == 200
-    assert '<title>Blackdog</title>' in response.content.lower()
+    content = response.content.decode().lower()
+    assert '<title>blackdog</title>' in content
+    assert content.startswith('<html>')
+    assert content.endswith('</html>')
