de6a4ba33baf1bdca00135a5ffb028adeb2bbab6
UncleGoogle <mieszkoziemowit@gmail.com>
2021-02-08 00:35:15 +0100

    adjust functional test to template html

--- dafipost/functional_test.py
+++ dafipost/functional_test.py
@@ -23,25 +23,11 @@ def test_index_non_authorized(browser, host):
     """
     Evo is here for the first time and wants to give a try to our application.
     """
-    # Evo go to main page
+    # Evo goes to home page
     browser.get(host)
 
     # He see the blackdog app title
-    assert 'blackdog' in browser.title.lower()
+    assert 'dafipost' in browser.title.lower()
     
     # He see 2 buttons to login and create new account
-    assert browser.find_element_by_xpath('//button[normalize-space()="Sign in"]')
-    assert browser.find_element_by_xpath('//button[normalize-space()="Sign up"]')
-
-
-@pytest.mark.functional
-def test_view_message_non_authorized(browser, host):
-    """
-    Mike is a new not authorized user.
-    He was invited by Tom to his see his public message.
-    """
-
-    # Mike goes to link shared by Tom
-    browser.get(host + '/tom/1')
-    
-    # TODO
+    assert browser.find_element_by_xpath("//*[contains(text(), 'Sign up')]")
